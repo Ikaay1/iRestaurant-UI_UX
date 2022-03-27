f611686fd92be8db4c1bcc893292215a2cf383c3
Mgbemele IkeChukwu <mgbemeleikechukwu@gmail.com>
2022-03-27 17:23:25 +0100

    changed something

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">Homat Junction, Ikorodu, Lagos</p>
+        <p className="p__opensans">+2349132459661</p>
+        <p className="p__opensans">+2348122120413</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -33,8 +33,8 @@ const Footer = () => (
         <h1 className="app__footer-headtext">Working Hours</h1>
         <p className="p__opensans">Monday-Friday:</p>
         <p className="p__opensans">08:00 am - 12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
-        <p className="p__opensans">07:00 am - 11:00 pm</p>
+        <p className="p__opensans">Saturday</p>
+        <p className="p__opensans">07:00 am - 11:00pm</p>
       </div>
     </div>
 
